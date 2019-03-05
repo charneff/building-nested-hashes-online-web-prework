bd9b2f0275cfb3c5c1dae2308b4c3baf5a23eba9
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-05 03:35:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,12 +5,21 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
+<<<<<<< HEAD
+=======
+      
+      #your key/value pairs here!
+>>>>>>> e4772d4562dad92e19e4eb1d1a4d9de3f4f43f61
       }, 
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
+<<<<<<< HEAD
+=======
+      #your key/value pairs here!
+>>>>>>> e4772d4562dad92e19e4eb1d1a4d9de3f4f43f61
       }
   }
   
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def third_challenge
   epic_tragedy = {
    :montague => {
@@ -38,3 +39,60 @@ def third_challenge
 
   
 end
+=======
+epic_tragedy = {
+  :montague => {
+    :patriarch => {  
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => [
+      {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+      },
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+    ]
+  },
+  :capulet => {
+    :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }
+}
+>>>>>>> e4772d4562dad92e19e4eb1d1a4d9de3f4f43f61
